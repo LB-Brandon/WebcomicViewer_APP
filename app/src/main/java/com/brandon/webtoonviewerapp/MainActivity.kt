cc8f90c4cb366651ec61e83e13a9182902b4e026
LB-Brandon <rmsdud623@gmail.com>
2023-11-22 18:20:28 +0900

    feat: Implement WebView for Testing
    * Add INTERNET permission in the manifest

--- app/src/main/java/com/brandon/webtoonviewerapp/MainActivity.kt
+++ app/src/main/java/com/brandon/webtoonviewerapp/MainActivity.kt
@@ -2,6 +2,7 @@ package com.brandon.webtoonviewerapp
 
 import android.os.Bundle
 import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -9,7 +10,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // webView component 가져오기
         val webView = findViewById<WebView>(R.id.webView)
-        webView.loadUrl("https://google/com")
+        // 아래 멤버는 실제로 setWebViewClient(WebViewClient client)이다
+        // 자바는 위 메서드를 호출해 사용한다
+        webView.webViewClient = WebViewClient()
+        // 웹뷰 내 자바스크립트 활성화
+        webView.settings.javaScriptEnabled = true
+
+        webView.loadUrl("https://www.google.com/")
     }
 }
